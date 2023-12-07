1988463ab8c264e6550dac778a334321a100bf25
sunalisekar <153157782+sunalisekar@users.noreply.github.com>
2023-12-07 10:37:33 +0530

    Add files via upload

---
+++ app/components/aboutus/aboutus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@({
+  selector: 'app-aboutus',
+  templateUrl: './aboutus.component.html',
+  template : Component`<h1> hello all </h1>`
+  styleUrls: ['./aboutus.component.scss']
+})
+export class AboutusComponent {
+
+}
